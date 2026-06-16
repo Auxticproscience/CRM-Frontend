239ed238cc7ca9a6f475fa4fa925bb9a1000669c
Aux TI <auxticproscience@gmail.com>
2026-06-16 08:05:06 -0500

    Fix Powe Bi URL

--- src/views/DashboardView.jsx
+++ src/views/DashboardView.jsx
@@ -1,5 +1,5 @@
 const POWERBI_URL =
-  "https://app.powerbi.com/reportEmbed?reportId=4aca6a04-237f-44d8-af63-e2c4624bffbc&autoAuth=true&ctid=d16ee8c2-074c-45cd-9bfe-18478c2d72c9"
+  "https://app.powerbi.com/view?r=eyJrIjoiYjAxYjY3MjEtYTE2ZS00ZDM2LThmNmMtZjlkMTA3MzJjNjFmIiwidCI6ImQxNmVlOGMyLTA3NGMtNDVjZC05YmZlLTE4NDc4YzJkNzJjOSJ9"
 
 export default function DashboardView() {
   return (
